069fabc68f9d6597ad9b78dada2881de845a24e0
Bo Fang <flyree@gmail.com>
2018-06-29 15:44:38 -0700

    update

--- faultinjection.cpp
+++ faultinjection.cpp
@@ -242,8 +242,8 @@ VOID FI_InjectFault_MEM_ECC(VOID * ip, VOID *memp, UINT32 size,UINT32 num, BOOL 
 {
 	if(fi_iterator == fi_inject_instance) {
 
-		fprintf(activationFile, ("Executing %p, memory %p, value %d, in hex %p\n",
-					ip, memp, * ((int*)memp), (VOID *)(*((int*)memp))));
+		fprintf(activationFile, "Executing %p, memory %p, value %d, in hex %x\n",
+					ip, memp, * ((int*)memp), (*((int*)memp)));
 
 
 		UINT8* temp_p = (UINT8*) memp;
@@ -264,8 +264,8 @@ VOID FI_InjectFault_MEM_ECC(VOID * ip, VOID *memp, UINT32 size,UINT32 num, BOOL 
 			*(temp_p + byte_num) = *(temp_p + byte_num) ^ (1U << offset_num);
 
 
-			fprintf(activationFile, ("Executing %p, memory %p, value %d, in hex %p\n",
-					ip, memp, * ((int*)memp), (VOID *)(*((int*)memp))));
+			fprintf(activationFile, "Executing %p, memory %p, value %d, in hex %x\n",
+					ip, memp, * ((int*)memp), (*((int*)memp)));
 			last_inject_bit = inject_bit;
 		}
 		fprintf(activationFile, "Activated: Memory injection\n");
